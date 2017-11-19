f260d0e8e21da3f212cd2f71c700398ae22b4988
Lukas Armstrong <Lucas.J.Armstrong@gmail.com>
2017-11-18 22:46:48 -0500

    changed listening port

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 5000;
 app.get('/', function(req, res)	{
 	res.render('pages/index.ejs');
 });
 
-app.listen('5000', function() {
+app.listen(port, function() {
 	console.log('it actuslly fookin worked');
 	});
