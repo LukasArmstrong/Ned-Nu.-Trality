8bbb8f74357001ecde18f5a27fc8d04ac9cea566
Lukas Armstrong <Lucas.J.Armstrong@gmail.com>
2017-11-19 00:51:20 -0500

    added CSS to site

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 5000;
 app.get('/', function(req, res)	{
 	res.render('pages/index.ejs');
 });
-
+app.use(express.static(__dirname + '/public'));
 app.listen(port, function() {
 	console.log('it actuslly fookin worked');
 	});
